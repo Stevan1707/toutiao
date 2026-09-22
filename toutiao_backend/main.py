154779169a3b64f3bb70629b3eba209fc880c6b5
Stevan1707 <1553868699@qq.com>
2026-09-22 11:04:54 +0800

    feat(api): 解决了跨域CORS问题

--- toutiao_backend/main.py
+++ toutiao_backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routers import news
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+# 通过全局配置cors来解决跨域问题
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # 默认允许所有源，生产需要配置源
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
